e2dba0a7c08260b0efdb37cf88fabf8fd098a623
Congbio <acongkplpe@gmail.com>
2022-12-22 14:08:21 +0700

    feat: update Modal relationship of table

--- app/Models/Conversation.php
+++ app/Models/Conversation.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+ 
 class Conversation extends Model
 {
     use HasFactory;
+    public function message()
+    {
+        return $this->hasMany(\App\Models\Message::class);
+    }
+    public function membersOfConversation()
+    {
+        return $this->hasMany(\App\Models\MembersOfConversation::class);
+    }
 }
