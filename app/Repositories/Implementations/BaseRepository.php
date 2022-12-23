a09e2c9b501ca0017c0915bb9052e811c761d971
NDK2509 <Dangkyqt2509@gmail.com>
2022-12-23 13:47:38 +0700

    fix: mistake of use class

--- app/Repositories/Implementations/BaseRepository.php
+++ app/Repositories/Implementations/BaseRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Implementations;
 
 use App\Repositories\Interfaces\IRepository;
 use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 
