bbf39990d349dcbe8e55daf3025415575864b1c6
Congbio <acongkplpe@gmail.com>
2022-12-29 13:46:36 +0700

    feat: update database(migration message,user).

--- database/factories/MessageFactory.php
+++ database/factories/MessageFactory.php
@@ -16,13 +16,12 @@ class MessageFactory extends Factory
      */
     public function definition()
     {
-        $string = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
-        $array=['video', 'image', 'record'];
+        $array=['video', 'image', 'record','text'];
         return [
             'con_id' =>rand(1000000,1000005),
-            'content' => $string,
+                'content' =>$this->faker->sentence($nbWords = 6, $variableNbWords = true),
             'user_id' =>rand(1000000,1000005),
-            'type' =>$array[rand(0,2)]
+            'type' =>$array[rand(0,3)]
 
         ];
     }
