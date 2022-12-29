e3ccecd0ebddfa241fa9078b7ab4750c5226547d
Congbio <acongkplpe@gmail.com>
2022-12-29 13:46:36 +0700

    feat: update database(migration message,user).

--- database/migrations/2022_12_21_081505_create_messages_table.php
+++ database/migrations/2022_12_21_081505_create_messages_table.php
@@ -20,11 +20,7 @@ return new class extends Migration
             $table->mediumText("content");
             $table->unsignedInteger("user_id");
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->enum('type', ['video', 'image', 'record']);  
-
-           
-
-
+            $table->enum('type', ['video', 'image', 'record','text']);
             $table->timestamps();
         });
     }
