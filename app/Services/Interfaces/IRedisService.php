49bab6a0ab987c5dbe631fba97b3bd17828af25a
Dang Xuan Vinh <vinh.dang23@student.passerellesnumeriques.org>
2023-01-04 12:09:07 +0700

    feat(add) : save OTP and Infor user to create user and Delete when created

--- app/Services/Interfaces/IRedisService.php
+++ app/Services/Interfaces/IRedisService.php
@@ -2,8 +2,20 @@
 
 namespace App\Services\Interfaces;
 
+use Illuminate\Support\Facades\Redis;
+
 interface IRedisService
 {
     public function setRefreshTokenWithUserId(int $userId, string $token): void;
     public function getRefreshTokenByUserId(int $userId): string|null;
+
+    public function setOtp($email,$otp):void;
+    public function getOtp($email):int|null;
+
+    public function setInfoRegis($dataRegis,$otp):void;
+
+    public function getInfoRegis($otp):mixed;
+
+    public  function deleteOtp($email): mixed;
+    public function  deleteInfor($otp): mixed;
 }
