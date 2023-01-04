c8a6e506eda2091f417a00aa05443d92b848b0a4
Dang Xuan Vinh <vinh.dang23@student.passerellesnumeriques.org>
2023-01-04 12:07:54 +0700

    feat(update): balade to sendEmail

--- resources/views/mail/otp_message.blade.php
+++ resources/views/mail/otp_message.blade.php
@@ -1,6 +1,6 @@
 <div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
     <div style="margin:50px auto;width:70%;padding:20px 0">
-        <p style="font-size:1.1em">Hi, {{$user->fullname}}</p>
+        <p style="font-size:1.1em">Hi, {{$user->full_name}}</p>
         <p>Thank you for choosing Your Brand. Use the following OTP to complete your Sign Up procedures. OTP is valid for 5 minutes</p>
         <h2 style="background: #00466a;margin: 0 auto;width: max-content;padding: 0 10px;color: #fff;border-radius: 4px;">{{$otp}}</h2>
         <p style="font-size:0.9em;">Regards,<br />KVLC</p>
