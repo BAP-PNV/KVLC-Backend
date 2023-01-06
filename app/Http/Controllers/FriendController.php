25da6df8ca08f5143ae4ee49a64d795956ecd9d0
Congbio <acongkplpe@gmail.com>
2023-01-06 14:04:13 +0700

    feat(add): search and unfriend controller

--- app/Http/Controllers/FriendController.php
+++ app/Http/Controllers/FriendController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+
+
 use App\Services\Interfaces\IFriendService;
+
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -12,6 +15,21 @@ class FriendController extends Controller
         private readonly IFriendService $friendService
     )
     {}
+
+
+    public function findFriend(Request $request){
+        $userId = $request->input('id');
+        $search = $request->input('q');
+        $findFriend = $this->friendService->findFriend($userId,$search);
+        return$findFriend;
+
+    }
+    public function unFriend(Request $request){
+        $userIdWant = $request->input('userIdWant');
+        $userIdBe = $request ->input('userIdBe');
+        $unFriend = $this->friendService->unFriend($userIdWant,$userIdBe);
+        return $unFriend;
+    }
     public function addFriend(Request $request)
     {
         $userWantAdd = $request->input("userWantAdd");
@@ -28,4 +46,6 @@ class FriendController extends Controller
 
 
     }
+
+
 }
