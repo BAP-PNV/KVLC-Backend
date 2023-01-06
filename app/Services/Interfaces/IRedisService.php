5137119b02de2412fb3a2999e4088edda169f781
NDK2509 <Dangkyqt2509@gmail.com>
2023-01-06 16:06:04 +0700

    feat (update): Auth and Redis services

--- app/Services/Interfaces/IRedisService.php
+++ app/Services/Interfaces/IRedisService.php
@@ -2,20 +2,25 @@
 
 namespace App\Services\Interfaces;
 
-use Illuminate\Support\Facades\Redis;
-
 interface IRedisService
 {
+    public function setActive(int $userId): void;
+    public function renewExpireTimeActive(int $userId): void;
     public function setRefreshTokenWithUserId(int $userId, string $token): void;
-    public function getRefreshTokenByUserId(int $userId): string|null;
-
+    public function getRefreshTokenByUserId(int $userId): ?string;
+    public function addActiveMemberToConversation(int $userId, int $conId): void;
+    public function getMemberOfConversation(int $conId): ?array;
+    public function existsConversation(int $conId): bool;
+    public function isUserInConversation(int $userId, int $conId): bool;
+    public function removeMemberOfConversation(int $userId, int $conId): void;
+    public function renewExpireTimeOfConversation(int $conId): void;
+    public function setFullName(int $userId, string $fullName): void;
+    public function getFullName(int $userId): ?string;
+    public function deleteFullName(int $userId): void;
     public function setOtp($email,$otp):void;
     public function getOtp($email):int|null;
-
     public function setInfoRegis($dataRegis,$otp):void;
-
     public function getInfoRegis($otp):mixed;
-
     public  function deleteOtp($email): mixed;
     public function  deleteInfor($otp): mixed;
 }
