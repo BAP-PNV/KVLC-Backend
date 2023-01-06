43a63bf80b04ca7095180b183440f2ff7f6a29c3
Congbio <acongkplpe@gmail.com>
2023-01-06 14:43:30 +0700

    feat(update): FriendService

--- app/Services/Interfaces/IFriendService.php
+++ app/Services/Interfaces/IFriendService.php
@@ -4,6 +4,9 @@ namespace App\Services\Interfaces;
 
 interface IFriendService
 {
-    public function addFriend($userWantAdd,$useBeAdded): mixed;
-    public function findFriend(int $userId,string $searText):mixed;
-    public function unFriend(int $userIdWant, int $userIdBe):mixed;
+    public function addFriend($userWantAdd, $useBeAdded): mixed;
+
+    public function findFriend(int $userId, string $searText): mixed;
+
+    public function unFriend(int $userIdWant, int $userIdBe): mixed;
+}
