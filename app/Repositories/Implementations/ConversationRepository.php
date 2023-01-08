2b806bb7ba7f56aeb426f6ec9e9a5e7a60a95369
NDK2509 <Dangkyqt2509@gmail.com>
2023-01-08 15:19:21 +0700

    feat (update): conversation and member repositories

--- app/Repositories/Implementations/ConversationRepository.php
+++ app/Repositories/Implementations/ConversationRepository.php
@@ -1,16 +1,19 @@
 <?php
+
 namespace App\Repositories\Implementations;
+
 use App\Models\Conversation;
-use App\Repositories\Implementations\BaseRepository;
 use App\Repositories\Interfaces\IConversationRepository;
-use Exception;
 use Illuminate\Database\Eloquent\Collection;
 
-class ConversationRepository extends BaseRepository implements IConversationRepository {
+class ConversationRepository extends BaseRepository implements IConversationRepository
+{
 
-    public function getModel():string{
+    public function getModel(): string
+    {
         return Conversation::class;
     }
+
     public function getMembersByConversationId(int $id): Collection
     {
         return $this->findById($id)->membersOfConversation;
