52391dd3b6ecc1b17a2aef26d0e36a75bb27f56f
Nguyễn Đăng Kỳ <87688740+NDK2509@users.noreply.github.com>
2023-01-09 21:48:35 +0700

    Delete IMessageService.php

--- app/Services/Interfaces/IMessageService.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Services\Interfaces;
-interface IMessageService {
-    public function send(int $conversationId, string $receiverId, string $content);
-    public function edit(int $conversationId, int $mesId, string $content);
-    public function delete(int $conversationId, int $mesId);
-}
