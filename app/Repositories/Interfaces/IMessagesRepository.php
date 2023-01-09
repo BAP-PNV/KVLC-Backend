3b8f6f4c7e19d483231466b8bf0fad7e2c6ff3b0
NDK2509 <Dangkyqt2509@gmail.com>
2023-01-09 12:24:51 +0700

    refactor: messages repository

--- app/Repositories/Interfaces/IMessagesRepository.php
+++ app/Repositories/Interfaces/IMessagesRepository.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Collection;
 interface IMessagesRepository extends IRepository
 {
     public function findMessagesByContent(int $conversationId, string $content): Collection|array|null;
-    public function nLatestMessagesOfConversation(int $conversationId, int $start = 0, int $end = 10): Collection|array|null;
+    public function getMessagesBy(int $conversationId, array $filter = ["offset" => 0, "limit" => 10], bool $toArray = false): Collection|array|null;
 
 }
