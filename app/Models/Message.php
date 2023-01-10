ccdabd9c62761c2e81eb0375818d5e89ec9df3a8
NDK2509 <Dangkyqt2509@gmail.com>
2023-01-11 02:00:25 +0700

    fix: add fillable attr for message model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+    protected $fillable = ["id", "con_id", "user_id", "content", "type", "created_at", "updated_at"];
     public function user()
     {
         return $this->belongsTo(\App\Models\User::class);
