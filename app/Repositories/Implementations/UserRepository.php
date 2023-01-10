20be4cf8128ffe645d6610206d48562c412336b2
Dang Xuan Vinh <vinh.dang23@student.passerellesnumeriques.org>
2023-01-10 13:26:16 +0700

    feat(): create find user

--- app/Repositories/Implementations/UserRepository.php
+++ app/Repositories/Implementations/UserRepository.php
@@ -28,4 +28,11 @@ class UserRepository extends BaseRepository implements \App\Repositories\Interfa
         ->Where("full_name", "LIKE","%$searchText%")
         ->orWhere("email", "LIKE","%$searchText%")->get();
     }
+    public function findUser(string $searchText): mixed
+    {
+        $listUser =$this->model->select("id", "full_name", "email")
+            ->Where("full_name", "LIKE","%$searchText%")
+            ->orWhere("email", "LIKE","%$searchText%")->get();
+        return sizeof($listUser)!=0 ? $listUser : [];
+    }
 }
